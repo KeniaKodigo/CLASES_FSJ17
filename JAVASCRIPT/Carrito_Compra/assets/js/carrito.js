769601be7557191d9062104794cfd876ac2c0d3c
KenyaPaiz <paizkenia5@gmail.com>
2023-05-17 21:57:53 -0600

    carrito de compra - ultima parte

--- JAVASCRIPT/Carrito_Compra/assets/js/carrito.js
+++ JAVASCRIPT/Carrito_Compra/assets/js/carrito.js
@@ -37,6 +37,32 @@ cargarEventos();
 function cargarEventos(){
     //addEvenlistener
     listaCursos.addEventListener('click', agregarCurso);
+
+    carrito.addEventListener('click', eliminarItem);
+
+    vaciarCarrito.addEventListener('click', () => {
+        Swal.fire({
+            title: '¿Estas seguro de vaciar el carrito?',
+            text: "Se eliminaran los cursos seleccionados!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Vaciar!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Se vacio el carrito!',
+                'Se eliminaron los cursos.',
+                'success'
+              ).then(function(){
+                arregloCarrito = [];
+                limpiarHTML(bodyCarrito);
+                limpiarHTML(footerCarrito);
+              })
+            }
+        })
+    })
 }
 
 /** tipos de funciones */
@@ -61,6 +87,13 @@ function agregarCurso(e){
         const cursoSeleccionado = e.target.parentElement.parentElement;
         //console.log(cursoSeleccionado);
         leerDatosCurso(cursoSeleccionado); //argumento es el valor que se le asigna al parametro
+        Swal.fire({
+            position: 'top-center',
+            icon: 'success',
+            title: 'Curso Agregado',
+            showConfirmButton: false,
+            timer: 1500
+        })
     }
 }
 
@@ -109,18 +142,22 @@ function leerDatosCurso(curso){ //parametro que hace referencia a los que el usu
 
 /** metodo donde se va mostrar la informacion del arreglo en la tabla */
 function carritoHTML(){
-
+    limpiarHTML(bodyCarrito);
+    let total = 0;
     arregloCarrito.map(curso => {
         //creando el elemento tr
         const fila = document.createElement('tr'); //<tr> <td></td></tr>
         //splice, slice
-
         //slice => metodo para eliminar un caracter de una cadena
         //$20.00 => 20.00 //double
         let act_precio = curso.precio.slice(1, -1); //eliminamos el primer caracter
         console.log(act_precio);
         //Number() => convierte el tipo de dato a int o double
         const subTotal = Number(act_precio * curso.cantidad);
+        /**
+         * mathRound() => aproxima decimales
+         * toFixed() => cuantos decimales queremos que se visualizen
+         */ 
         fila.innerHTML = `
             <td>
                 <img src="${curso.imagen}" width="100">
@@ -128,14 +165,99 @@ function carritoHTML(){
             <td>${curso.titulo}</td>
             <td>${curso.precio}</td>
             <td>${curso.cantidad}</td>
-            <td>$${subTotal}</td>
+            <td>$${subTotal.toFixed(2)}</td>
             <td>
                 <a href="#" class="borrar-item" data-id="${curso.id}">X</a>
             </td>
         `;
-
+        total += subTotal;
         //agregando el elemento tr al html (tbody)
         bodyCarrito.appendChild(fila); //appendChild() = agregamos hijos al contenedor que le indiquemos
     })
+
+    //asignacion de fila para el tfoot
+    limpiarHTML(footerCarrito);
+    const fila_foot = document.createElement('tr');
+    fila_foot.innerHTML = `
+        <td colspan="4">Total del Pedido</td>
+        <td>$${total}</td>
+    `;
+
+    footerCarrito.appendChild(fila_foot);
 }
 
+/** metodo para eliminar hijos repetitivos  */
+function limpiarHTML(contenedorPadre){
+    while(contenedorPadre.firstChild){
+        contenedorPadre.removeChild(contenedorPadre.firstChild);
+    }
+}
+
+/** metodo para confirmar el pedido */
+function guardarPedido(){
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success ms-2',
+          cancelButton: 'btn btn-danger',
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: '¿Estas Seguro de Guardar el Pedido?',
+        text: "El pedido se cargara a tu tarjeta!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Guardar Pedido',
+        cancelButtonText: 'Cancelar!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Ha sido un exito!',
+            'Pedido se ha guardado correctamente',
+            'success'
+          ).then(function(){
+            //redirecciona a una pagina
+            window.location = "index.html";
+          })
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'Puedes seguir agregando mas productos :)',
+            'error'
+          )
+        }
+    })
+}
+
+
+/** metodo para eliminar un item del pedido */
+function eliminarItem(e){
+    if(e.target.classList.contains('borrar-item')){
+        //obtenemos el id del curso seleccionado
+        const cursoId = e.target.getAttribute('data-id');
+
+        //filtrando los cursos que sean diferentes al id seleccionado
+
+        //map(), filter()
+        arregloCarrito = arregloCarrito.filter(curso => curso.id !== cursoId);
+        carritoHTML();
+    }
+}
+
+/*arregloCarrito = [
+    {
+        id: "1",
+        titulo: "ghbjk",
+        precio: "$15.00"
+    },
+    {
+        id: "2",
+        titulo: "ghbjk",
+        precio: "$17.00"
+    },
+]*/
